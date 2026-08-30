6fcb8c58c9b6d67b40615bc13b26fa00486e9b84
konstasil <konstasil777@gmail.com>
2026-08-30 09:42:29 +0300

    Fix NeoForge 26.x API: config screen via IConfigScreenFactory
    - Removed TotemSkinModMenu.java (RegisterConfigScreensEvent no longer exists)
    - Added ModContainer parameter to TotemSkin constructor
    - Register config screen via container.registerExtensionPoint(IConfigScreenFactory.class, ...)

--- TotemSkin/src/main/java/com/konstasiil/totemskin/TotemSkin.java
+++ TotemSkin/src/main/java/com/konstasiil/totemskin/TotemSkin.java
@@ -1,7 +1,9 @@
 package com.konstasiil.totemskin;
 
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,8 +12,11 @@ public class TotemSkin {
     public static final String MOD_ID = "totemskin";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public TotemSkin(IEventBus modEventBus) {
+    public TotemSkin(IEventBus modEventBus, ModContainer container) {
         LOGGER.info("TotemSkin initialized");
         TotemTextureManager.init();
+
+        container.registerExtensionPoint(IConfigScreenFactory.class,
+                (modContainer, parent) -> TotemSkinConfigScreen.createScreen(parent));
     }
 }
